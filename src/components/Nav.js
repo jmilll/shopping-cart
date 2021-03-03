816965536ed03523ce60a06056219fd2e301db25
jmilll <jwmiller.design@gmail.com>
2021-03-03 15:54:41 -0500

    Initial Commit

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav className='nav'>
+            <h3>Logo/Home</h3>
+            <ul className='nav-links'>
+                <li>Shop</li>
+                <li>Cart</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
