b63a12525e20b70b2039569e67f9ac4022a687b1
jmilll <jwmiller.design@gmail.com>
2021-03-03 15:55:07 -0500

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 //import logo from './logo.svg';
 import './App.css';
+import Cart from './components/Cart';
+import Nav from './components/Nav';
+import Shop from './components/Shop';
+import Home from './components/Home';
+import {BrowserRouter as Router, Switch, Route,} from 'react-router-dom'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Home</h1>
-    
-    </div>
+    <Router>
+      <div className="App">
+        {/* <h1>Home</h1> */}
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route path="/shop" component={Shop} />
+        <Route path="/cart" component={Cart} />
+        {/* <Cart />
+        <Shop /> */}
+      </div>
+    </Router>
   );
 }
 
