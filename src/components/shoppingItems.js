7100eca738c7776df86fbef89bd65cc45059d54e
jmilll <jwmiller.design@gmail.com>
2021-03-11 17:47:19 -0500

    Initial Commit

---
+++ src/components/shoppingItems.js
@@ -0,0 +1,63 @@
+const items = [
+    {
+        id: 'big-leafy-boi',
+        name: 'Big Leafy Boi',
+        imgSource: 'big-leafy-boi.jpg',
+        quantity: 1,
+        price: 20,
+    },
+    {
+        id: 'small-leafy-boi',
+        name: 'Small Leafy Boi',
+        imgSource: 'small-leafy-boi.jpg',
+        quantity: 1,
+        price: 10,
+    },
+    {
+        id: 'medium-droopy-tree',
+        name: 'Medium Droopy Tree',
+        imgSource: 'medium-droopy-tree.jpg',
+        quantity: 1,
+        price: 50,
+    },
+    {
+        id: 'tall-pointy-tree',
+        name: 'Tall PointyTree',
+        imgSource: 'tall-pointy-tree.jpg',
+        quantity: 1,
+        price: 70,
+    },
+]
+
+export default items
+
+// export default [
+//     {
+//         id: 'big-leafy-boi',
+//         name: 'Big Leafy Boi',
+//         imgSource: 'big-leafy-boi.jpg',
+//         quantity: 1,
+//         price: 20,
+//     },
+//     {
+//         id: 'small-leafy-boi',
+//         name: 'Small Leafy Boi',
+//         imgSource: 'small-leafy-boi.jpg',
+//         quantity: 1,
+//         price: 10,
+//     },
+//     {
+//         id: 'medium-droopy-tree',
+//         name: 'Medium Droopy Tree',
+//         imgSource: 'medium-droopy-tree.jpg',
+//         quantity: 1,
+//         price: 50,
+//     },
+//     {
+//         id: 'tall-pointy-tree',
+//         name: 'Tall PointyTree',
+//         imgSource: 'tall-pointy-tree.jpg',
+//         quantity: 1,
+//         price: 70,
+//     },
+// ]
