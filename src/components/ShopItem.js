2e59a2035b390c7e35def0c05fd7003cee3db4e8
jmilll <jwmiller.design@gmail.com>
2021-03-14 17:05:58 -0400

    Initial Commit

---
+++ src/components/ShopItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ShopItem(props) {
+    const { a } = props
+
+    return (
+        <div>
+            {a}
+        </div>
+    )
+}
+
+export default ShopItem
