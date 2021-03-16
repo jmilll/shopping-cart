491de1edc9ac782678672e77c420526acc4b1997
jmilll <jwmiller.design@gmail.com>
2021-03-16 19:02:21 -0400

    home styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div className='home'>
-            <h1>Home Page</h1>
-            <p>homie pagee</p>
+            <Link className='item-link-shop' to={`/shop`}>
+                <button className='btn enter'>Step Inside The Greenhouse</button>
+                
+            </Link>
+            {/* <img className='home-image' src='https://www.gardeningknowhow.com/wp-content/uploads/2013/10/greenhouse.jpg'/> */}
+            
+            <img className='image' src='https://yardyum.com/storage/app/media/Greenhouse%20with%20Vegetables.jpg'/>
         </div>
     )
 }
