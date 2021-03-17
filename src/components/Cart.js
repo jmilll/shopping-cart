36139987759fec8e9a7e8d9c6095e0f28c8c93ad
jmilll <jwmiller.design@gmail.com>
2021-03-17 14:43:31 -0400

    cart done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,11 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Cart = (props) => {
-    //console.log('cart props below')
-    //console.log(props)
     const { cart, changeQuantity, removeFromCart } = props
-    console.log('cart deconstructed props below')
-    console.log(cart)
+    // console.log('cart deconstructed props below')
+    // console.log(cart)
 
     let cartTotal = 
         cart.map(item => item.price * item.quantity)
@@ -18,70 +16,74 @@ const Cart = (props) => {
   //arr.reduce(callback( accumulator, currentValue, [, index[, array]] )[, initialValue])
   //const cartQuantity = cart.reduce((accumulator, currentValue) => accumulator + currentValue.quantity, 0)
 
+    if (!cart.length) {
+        return (
+            <section className='page-container cart'>
+                <div className='cart'>
+                    <h1>MY SHOPPING CART</h1>
+                    <span className='header'/>
+                    <div className='empty-cart'>
+                        <p>It's Empty...</p>
+                        <p>You dont like plants?</p>
+                        <Link className='empty-link' to='/shop'>
+                            <button className='btn empty'>Get Yourself Some Plant Friends</button>
+                        </Link>
+                    </div>
+                </div>
+            </section>
+        )
+    } else {
+    
     return (
         <section className='page-container cart'>
 
             <div className='cart'>
-            <h1>MY SHOPPING CART</h1>
-            <span className='header'>
-                {/* <p>Item</p>
-                <p>Quantity</p>
-                <p>Subtotal</p> */}
-            </span>
+                <h1>MY SHOPPING CART</h1>
 
-            {cart.map(item => {
-                return(
-                    <div className='cart-item' key={item.id}>
-                        {/* <div className='cart-item-info'> */}
+                <span className='header'/>
+
+                {cart.map(item => {
+                    return(
+                        <div className='cart-item' key={item.id}>
                             <div className='img-container-cart'>
                                 <img className='image cart' src={item.imgSource} alt={item.id} />
                             </div>
 
-
                             <Link className='item-link cart' to={`/shop/${item.id}`}>
                                 <p>{item.name}</p>
-                                
                             </Link>
-                        {/* </div> */}
-                        {/* <div>
-                            <Link className='item-link' to={`/shop/${item.id}`}>
-                                <p>{item.name}</p>
-                            </Link>
-                            <p className='cart-item-price'>${item.price}</p>
-                        </div> */}
-                        
 
-                        <div className='quantity-wrapper'>
-                            <button className='btn qty' onClick={() => changeQuantity(item.id, -1)} disabled={item.quantity <2}>-</button>
-                            <p className='quantity'>{item.quantity}</p>
-                            <button className='btn qty' onClick={() => changeQuantity(item.id, 1)}>+</button>
+                            <div className='quantity-wrapper'>
+                                <button className='btn qty' onClick={() => changeQuantity(item.id, -1)} disabled={item.quantity <2}>-</button>
+                                <p className='quantity'>{item.quantity}</p>
+                                <button className='btn qty' onClick={() => changeQuantity(item.id, 1)}>+</button>
+                            </div>
+
+                            <div>
+                                <p className='sub-total'>Subtotal ${itemTotal(item.price, item.quantity)}</p>
+                                <p className='sub-price'>(${item.price} / each)</p>
+                            </div>
+
+                            <button className='btn remove' onClick={() => removeFromCart(item.id)}>Remove</button>
                         </div>
+                    )
+                })}
 
-                        {/* <p className='sub-total'>Subtotal ${itemTotal(item.price, item.quantity)}</p> */}
+                <div className='cart-total'>
+                    <Link className='back-link' to='/shop'>
+                        <button className='btn back'>Add More Plants?</button>
+                    </Link>
 
-                        <div>
-                            <p className='sub-total'>Subtotal ${itemTotal(item.price, item.quantity)}</p>
-                            <p className='sub-price'>(${item.price} / each)</p>
-                        </div>
-                        <button className='btn remove' onClick={() => removeFromCart(item.id)}>Remove</button>
-                    </div>
-                )
-            })}
+                    <h2>TOTAL ${cartTotal}</h2>
 
-            <div className='cart-total'>
-                <Link className='back-link' to='/shop'>
-                    <button className='btn back'>Add More Plants?</button>
-                </Link>
-
-                <h2>TOTAL ${cartTotal}</h2>
-
-                <Link className='checkout-link' to='/checkout'>
-                    <button className='btn checkout'>Checkout</button>
-                </Link>
-            </div>
+                    <Link className='checkout-link' to='/checkout'>
+                        <button className='btn checkout'>Checkout</button>
+                    </Link>
+                </div>
             </div>
         </section>
     )
+    }
 }
 
 export default Cart
