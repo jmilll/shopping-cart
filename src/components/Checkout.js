d4eb34cf9225ee54b2f92879db0e3aa5fba709eb
jmilll <jwmiller.design@gmail.com>
2021-03-17 15:22:27 -0400

    checkout done

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Checkout() {
     return (
-        <div>
-            CHECKOUT PROCESS NOT CREATED
-        </div>
+        <section className='page-container checkout'>
+            <div className='checkout'>
+                <div className='empty-cart'>
+                    <p>Well, thanks for trying!</p>
+                    <p>This is just a demo</p>
+                    <Link className='empty-link' to='/shop'>
+                        <button className='btn empty'>Back To Shopping</button>
+                    </Link>
+                </div>
+            </div>
+        </section>
     )
 }
 
